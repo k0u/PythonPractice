c2d0a63fb8796eb8d5f83f930c7f64dbcea6537f
k0u <k0u.60512109@gmail.com>
2023-11-09 22:17:39 +0900

    ファイル整理スクリプトの作成

--- readymade_sample.py
+++ readymade_sample.py
@@ -2,16 +2,37 @@
 ファイルを整理整頓する
 作業ディレクトリに散財するファイルを分類
 拡張子で判別
-.py
 .txt
 .log
-上記以外の拡張子
 サブディレクトリに分けて移動する
-scripts textfiles logs others
+textfiles logs
 """
 import glob
-import re
+import os
+import shutil
 
-for name in glob.glob('./'):
-    if re.search(r"^.*\.py", name) :
-        print("match")
+def move_files(target_file, new_path):
+    l = glob.glob(target_file)
+    if l:
+        # ディレクトリを作成
+        os.makedirs(new_path, exist_ok=True)
+        for file in l:
+
+            print(file)
+            # ファイル移動
+            try:
+                shutil.move(file, new_path)
+            except Exception:
+                print(f"移動先に同名のファイルが存在します。＝＞{file}")
+    else:
+        print(f"該当ファイルなし=>{target_file}")
+
+def sort_textfiles():
+    move_files("./*.txt", "./textfiles")
+
+def sort_logfiles():
+    move_files("./*.log", "./logs")
+
+
+sort_textfiles()
+sort_logfiles()
