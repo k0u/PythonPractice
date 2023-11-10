be08cdc24e629f01c70c878979406189af611eb5
k0u <k0u.60512109@gmail.com>
2023-11-11 03:56:43 +0900

    日付時刻の相互変換

--- tokyo_olympics.py
+++ tokyo_olympics.py
@@ -11,3 +11,15 @@
 相互変換
 相互に相互変換な方法を覚えておく
 """
+import datetime
+
+date_string = "2023-11-11 12:30:00"
+date_object = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+print(f"{date_object} type:{type(date_object)}")
+
+
+# 現在時刻を取得
+current_time = datetime.datetime.now()
+
+current_time_string = current_time.strftime("%Y-%m-%d %H:%M:%S")
+print(f"{current_time_string} type:{type(current_time_string)}")
